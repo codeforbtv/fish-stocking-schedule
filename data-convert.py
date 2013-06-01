6cd7a555d4e1e403bebcecdd558e425d08075bc9
Matt Parrilla <matthew.parrilla@gmail.com>
2013-06-01 18:33:06 -0400

    Data normalized and ready to rock

--- data-convert.py
+++ data-convert.py
@@ -2,106 +2,187 @@
 
 import csv
 
+def fish_index(row):
+    """ This function is used by first_step() to create the new csv rows"""
+    species = row[3]
+    if row[7]:
+        if species == 'BKT':
+            x = [row[1], row[0], row[5], row[7], '','','','','','','','','','']
+        elif species == 'LAT':
+            x = [row[1], row[0],'','', row[5], row[7], '','','','','','','','']
+        elif species == 'RBT':
+            x = [row[1], row[0], '','','','', row[5], row[7], '','','','','','']
+        elif species == 'STT':
+            x = [row[1], row[0], '','','','','','',row[5], row[7], '','','','']
+        elif species == 'BNT':
+            x = [row[1], row[0], '','','','','','','','',row[5], row[7], '','']
+        elif species == 'LAS':
+            x = [row[1], row[0], '','','','','','','','','','', row[5], row[7] ]
+        else:
+            x = 'fail'
+        return x
+    else:
+        pass
 
-# below for incorporating on, ha data
+def create_rows():
+    """Prior to this step, I used the excel "substitute' function to convert
+    all town names with /, +, ' to', etc. to hyphens.
+    This function creates the new csv rows and breaks multi-town entries
+    into 2 (or 3), dividing the fish equally among towns"""
 
-#f = csv.reader(open('../../Desktop/fema-summed.csv'))
-#z = csv.reader(open('../../Desktop/zip-master.csv'))
-#
-#fema = [l for l in f]
-#zips = [l for l in z]
-#
-#for line in zips:
-#    if line[3] == 'TRUE': # if town is a parent
-#        fema.append([line[0], line[1], line[3],'' ,'' ,'' , line[2]])
-#        line[0] = 'x'
-#    else:
-#        for town in fema:
-#            if line[0] == town[0]:
-#                town[1] = line[1]
-#                line[0] = 'x'
-#                if line[3]:
-#                    town[2] = line[3]
-#
-#    if line[0] != 'x':
-#        fema.append([ line[0], line[1] ])
-#
-#writer = csv.writer(open('../../Desktop/new.csv', 'w'))
-#writer.writerows(fema)
+    f = csv.reader(open('fish-stocking1.csv', 'rU'))
+    fish = [l for l in f]
 
+    new_list = []
+    multi_town = ['-', '+']
+    for row in fish:
+        if row[5]:
+            x = False
+            triple_town = False
+            for i in range(len(row[1])):
+                if row[1][i] in multi_town:
+                    j = i + 1
+                    x = row[1][:i]
+                    y = row[1][j:]
+                    multi_town_fish = int(row[5])/2
+                    for h in range(len(y)):
+                        if y[h] in multi_town:
+                            triple_town = True
+                            g = h + 1
+                            z = y[:h]
+                            w = y[g:]
+                            multi_town_fish = int(row[5])/3
+            if x:
+                row[1] = x
+                if triple_town:
+                    row[5] = multi_town_fish
+                    new_list.append(fish_index(row))
+                    row[1] = z
+                    new_list.append(fish_index(row))
+                    row[1] = w
+                    new_list.append(fish_index(row))
+                else:
+                    row[5] = multi_town_fish
+                    new_list.append(fish_index(row))
+                    row[1] = y
+                    new_list.append(fish_index(row))
+            else:
+                new_list.append(fish_index(row))
 
-#below for combining towns in pa data
+    writer = csv.writer(open('fish-stocking-1.csv', 'w'))
+    writer.writerows(new_list)
 
-#f = csv.reader(open('../../Desktop/public-assistance.csv'))
-#
-#pa = [l for l in f]
-#
-#fema = []
-#
-#for i in pa:
-#
-#    entry = []
-#    for j in pa:
-#        if i[0].strip() == j[0].strip():
-#            print i, j
-#            entry.append([j[0].strip(), j[2]])
-#
-#    if len(entry) == 1:
-#        if entry[0] not in fema:
-#            fema.append(entry[0])
-#    elif len(entry) > 1:
-#        town_funds = 0
-#        for x in entry:
-#            town_funds += float(x[1])
-#        new_entry = [entry[0][0].strip(), town_funds]
-#        if new_entry not in fema:
-#            fema.append(new_entry)
-#    else:
-#        print 'huh!?'
-#
-#writer = csv.writer(open('../../Desktop/pa.csv', 'w'))
-#writer.writerows(fema)
+def combine_waterways():
+    """This function creates one entry per town, per waterway, adding all of
+    the fish totals and creating the new average"""
 
+    f = csv.reader(open('fish-stocking1.csv', 'rU'))
+    fish = [l for l in f]
+    new_list = []
 
-# for combining ihp and pa
+    for i in fish:
+        town_water = [0]*14
+        town_water[0] = i[0]
+        town_water[1] = i[1]
+        for j in fish:
+            if i[0] == j[0] and i[1] == j[1]:
+                for x in range(len(i)):
+                    if x > 1 and j[x]:
+                        if not x % 2:
+                            num_fish = int(j[x])
+                            town_water[x] += num_fish
+                        else:
+                            idx = x - 1
+                            len_of_row = float(j[x]) * float(j[idx])
+                            prev_len_of_fish = float(town_water[x]) * (float(town_water[idx])-float(j[idx]))
+                            avg_len = (len_of_row + prev_len_of_fish) / float(town_water[idx])
+                            town_water[x] = avg_len
+        if town_water not in new_list:
+            new_list.append(town_water)
 
-#x = csv.reader(open('../../Dropbox/irene-csv/irene-funding.csv', 'rU'))
-#
-#y = csv.reader(open('../../Dropbox/irene-csv/pa.csv', 'rU'))
-#
-#irene = [l for l in x]
-#pa = [l for l in y]
-#
-#for town in pa:
-#    for child in irene:
-#        if town[0] == child[0] and child[2] != 'TRUE':
-#            child.append(town[1])
-#            town.append('x')
-#
-#for i in pa:
-#    if i[-1] != 'x':
-#        entry = [0] * 15
-#        entry[0] = i[0]
-#        entry[14] = i[1]
-#        irene.append(entry)
-#
-#writer = csv.writer(open('../../Dropbox/irene-csv/ihp-pa.csv', 'w'))
-#writer.writerows(irene)
+    writer = csv.writer(open('fish-stocking2.csv', 'w'))
+    writer.writerows(new_list)
 
 
-x = csv.reader(open('../../Dropbox/irene-csv/ihp-pa.csv', 'rU'))
+def create_master():
+    """Creates one 'master' entry per town, summing fish totals, ignoring length"""
 
-fema = [l for l in x]
+    f = csv.reader(open('fish-stocking2.csv', 'rU'))
+    fish = [l for l in f]
 
-for town in fema:
-    if town[2] == 'TRUE':
-        pa = 0
-        for i in fema:
-            if town[0] == i[2]:
-                if i[13]:
-                    pa += float(i[13])
-        town[13] = pa
+    new_list = []
+    for row in fish:
+        town_water = [0]*14
+        town_water[1] = 'MASTER'
+        town_water[0] = row[0]
+        for other in fish:
+            if row[0] == other[0]:
+                idx = [2,4,6,8,10,12]
+                for i in idx:
+                    if other[i]:
+                        num_fish = int(other[i])
+                        town_water[i] += num_fish
+
+        if town_water not in new_list:
+            new_list.append(town_water)
+
+    for row in fish:
+        new_list.append(row)
+
+    writer = csv.writer(open('fish-stocking3.csv', 'w'))
+    writer.writerows(new_list)
 
 
-writer = csv.writer(open('../../Dropbox/irene-csv/irene-fema-totals.csv', 'w'))
-writer.writerows(fema)
+def match_zipcode():
+    """Matches fisheries data to vt.json zipcode data"""
+    f = csv.reader(open('fish-stocking3.csv', 'rU'))
+    fish = [l for l in f]
+
+    z = csv.reader(open('vt-zipcode.csv', 'rU'))
+    zips = [l for l in z]
+
+    new_list = []
+    for row in fish:
+        row.insert(1, '')
+        if row[2] == 'MASTER':
+            for town in zips:
+                if row[0].strip() == town[0]:
+                    if town[1]:
+                        row[1] = town[1]
+                    elif town[0] == town[2]:
+                        continue
+                    else:
+                        row[1] = town[2]
+            new_list.append(row)
+        else:
+            new_list.append(row)
+
+    writer = csv.writer(open('fish-stocking4.csv', 'w'))
+    writer.writerows(new_list)
+
+def make_parent():
+    """Assigns a parent polygon to all child towns (which have multiple
+    unique entries by watereway"""
+
+    f = csv.reader(open('fish-stocking4.csv', 'rU'))
+    fish = [l for l in f]
+
+    new_list = []
+    for row in fish:
+        if row[2] != 'MASTER':
+            for other in fish:
+                if row[0] == other[0] and len(other[1]) != 4 and other[2] == 'MASTER':
+                    row[1] = other[1]
+                elif row[0] == other[0] and len(other[1]) == 4 and other[2] == 'MASTER':
+                    row[1] = other[0]
+        if len(row[1]) != 4 and row[2] == 'MASTER':
+            continue
+        else:
+            new_list.append(row)
+    writer = csv.writer(open('fish-stocking-final.csv', 'w'))
+    writer.writerows(new_list)
+
+combine_waterways()
+create_master()
+match_zipcode()
+make_parent()
