30aaf340996576e4daa21055cd310cd2bccd1ad2
Joe di Stefano <joeydi@okaypl.us>
2013-06-01 16:42:38 -0400

    add data field switching and polygon zooming

--- map.js
+++ map.js
@@ -1,14 +1,48 @@
 var Fish = Fish || {};
 
+Fish.csv_file = 'irene-funding-test.csv';
+Fish.json_file = 'vt.json';
+Fish.cloudmade_api_key = '8ee2a50541944fb9bcedded5165f09d9';
+Fish.data_field = 'fema';
+Fish.default_color = '#ccc';
+
+Fish.fill_algorithm = function(d) {
+    var val = d.properties[Fish.data_field];
+
+    console.log(val);
+
+    if (val) {
+        return color(val);
+    } else {
+        return Fish.default_color;
+    }
+}
+
+Fish.get_feature_bounds = function(feature) {
+    var coordinates = feature.geometry.coordinates[0],
+        bounds;
+
+    for (var i = 0; i < coordinates.length; i++) {
+        var latlng = coordinates[i],
+            latlng = new L.LatLng(latlng[1], latlng[0]);
+
+        if (i == 0) {
+            bounds = new L.LatLngBounds(latlng);
+        }
+
+        bounds.extend(latlng);
+    }
+
+    return bounds;
+}
+
 // Initialize leaflet map
-var cloudmade_api_key = '8ee2a50541944fb9bcedded5165f09d9',
-    cloudmade_attribution,
-    map = new L.Map('map', {
+var map = new L.Map('map', {
       center: [43.8, -72.7],
       zoom: 8
     });
 
-map.addLayer(new L.TileLayer('http://{s}.tile.cloudmade.com/'+ cloudmade_api_key +'/998/256/{z}/{x}/{y}.png'));
+map.addLayer(new L.TileLayer('http://{s}.tile.cloudmade.com/'+ Fish.cloudmade_api_key +'/998/256/{z}/{x}/{y}.png'));
 
 var svg = d3.select(map.getPanes().overlayPane).append("svg"),
     g = svg.append("g").attr("class", "leaflet-zoom-hide");
@@ -27,79 +61,71 @@ var yAxis = d3.svg.axis()
     .tickValues(color.domain())
     .orient("right");
 
-var json_loaded = function(error, vt) {
-    // console.log(topology);
+Fish.json_loaded = function(error, topology) {
+    Fish.topology = topology;
+    Fish.draw_features();
+}
+
+Fish.draw_features = function() {
+    var topology = Fish.topology;
 
     for (var i = 0; i < Fish.data.length; i++) {
-        var dataTown = Fish.data[i].town;
-        var dataZip = "0" + Fish.data[i].zipcode;
-        var dataFEMA = parseFloat(Fish.data[i].fema);
+        var zip = "0" + Fish.data[i].zipcode;
 
-        for (var j = 0; j < vt.objects.vermont.geometries.length; j++) {
-            var jsonZip = vt.objects.vermont.geometries[j].properties.zipcode;
+        for (var j = 0; j < topology.objects.vermont.geometries.length; j++) {
+            var jsonZip = topology.objects.vermont.geometries[j].properties.zipcode;
 
-            if (dataZip == jsonZip) {
-                vt.objects.vermont.geometries[j].properties.town = dataTown;
-                vt.objects.vermont.geometries[j].properties.fema = dataFEMA;
-
+            if (zip == jsonZip) {
+                topology.objects.vermont.geometries[j].properties = Fish.data[i];
                 break;
             }
         }
     }
 
-    var collection = topojson.feature(vt, vt.objects.vermont);
+    var collection = topojson.feature(topology, topology.objects.vermont);
+
+    Fish.collection = collection;
     // console.log(collection);
 
     var bounds = d3.geo.bounds(collection),
         path = d3.geo.path().projection(project);
 
-    var feature = g.selectAll("path")
+    Fish.feature = g.selectAll("path")
         .data(collection.features)
-        .enter().append("path")
-        .attr("d", path)
-        .style("fill", function(d) {
-            var fema = d.properties.fema;
+        .enter().append('path')
+        .attr('d', path)
+        .style('fill', Fish.fill_algorithm);
 
-            if (fema) {
-                return color(fema);
-            } else {
-                return "#ccc";
-            }
-        })
+    Fish.feature.on('mouseover', function(d) {
+        var xPosition = d3.mouse(this)[0];
+        var yPosition = d3.mouse(this)[1] - 30;
 
-        .on("mouseover", function(d) {
-            var xPosition = d3.mouse(this)[0];
-            var yPosition = d3.mouse(this)[1] - 30;
+        g.append("text")
+            .attr("id", "tooltip")
+            .attr("x", xPosition)
+            .attr("y", yPosition)
+            .attr("text-anchor", "middle")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "11px")
+            .attr("font-weight", "bold")
+            .attr('fill', "black")
+            .text(d.properties.town);
 
-            svg.append("text")
-                .attr("id", "tooltip")
-                .attr("x", xPosition)
-                .attr("y", yPosition)
-                .attr("text-anchor", "middle")
-                .attr("font-family", "sans-serif")
-                .attr("font-size", "11px")
-                .attr("font-weight", "bold")
-                .attr("fill", "black")
-                .text(d.properties.town);
+        d3.select(this).style('fill', "#509e2f");
+    });
 
-            d3.select(this)
-            .style("fill", "#509e2f");
-        })
-        .on("mouseout", function(d) {
-            d3.select("#tooltip").remove();
+    Fish.feature.on('mouseout', function(d) {
+        d3.select("#tooltip").remove();
+        if (d3.select(this).attr('class') !== 'active') {
+            d3.select(this).transition().duration(250).style('fill', Fish.fill_algorithm);
+        }
+    });
 
-            d3.select(this)
-            .transition()
-            .duration(250)
-            .style("fill", function(d) {
-            var fema = d.properties.fema;
-
-            if (fema) {
-                return color(fema);
-            } else {
-                return "#ccc";
-            }
-        });
+    Fish.feature.on('click', function(d) {
+        var bounds = Fish.get_feature_bounds(d);
+        map.fitBounds(bounds.pad(0.25));
+        d3.select('.active').attr('class', '').style('fill', Fish.fill_algorithm);
+        d3.select(this).attr('class', 'active').style('fill', "#509e2f");
     });
 
     map.on('viewreset', reset);
@@ -117,7 +143,7 @@ var json_loaded = function(error, vt) {
 
         g.attr("transform", "translate(" + -bottomLeft[0] + "," + -topRight[1] + ")");
 
-        feature.attr("d", path);
+        Fish.feature.attr("d", path);
     }
 
     // Use Leaflet to implement a D3 geographic projection.
@@ -127,17 +153,34 @@ var json_loaded = function(error, vt) {
     }
 }
 
-var load_json = function() {
-    d3.json('vt.json', json_loaded);
+Fish.redraw_features = function() {
+    Fish.feature.transition().duration(250).style('fill', Fish.fill_algorithm);
 }
 
-var csv_loaded = function(data) {
-    Fish.data = data;
-    load_json();
+Fish.load_json = function() {
+    d3.json(Fish.json_file, Fish.json_loaded);
 }
 
-var load_csv = function() {
-    d3.csv('irene-funding.csv', csv_loaded);
+Fish.load_csv = function() {
+    d3.csv(Fish.csv_file, function(data) {
+        Fish.data = data;
+        Fish.load_json();
+    });
 }
 
-load_csv();
+Fish.init_species_menu = function() {
+    var menu = $('#species');
+
+    menu.find('li').click(function() {
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+
+        Fish.data_field = $(this).data('field');
+        Fish.redraw_features();
+    });
+};
+
+$(document).ready(function() {
+    Fish.load_csv();
+    Fish.init_species_menu();
+});
